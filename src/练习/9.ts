4566f418aec1c026e83483c667c0f54c665963d4
bei <bei@beideMacBook-Pro.local>
2023-04-05 10:12:45 +0800

    init

---
+++ src/练习/9.ts
@@ -0,0 +1,15 @@
+// Implement a generic TupleToUnion<T> which covers the values of a tuple to its values union.
+
+// For example
+
+type Arr = ['1', '2', '3']
+
+type Test = TupleToUnion<Arr> // expected to be '1' | '2' | '3'
+
+// type TupleToUnion<T extends unknown[]> = T[number]
+
+type TupleToUnion<T> = T extends Array<infer U> ? U : never;
+
+export {}
+
+// 快排
